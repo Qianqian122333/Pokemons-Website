a0d81d9af012d7d33b3ac8ebcd23f731fea56b17
Qianqian Wei <qianqianwei112233@gmail.com>
2026-01-19 14:18:20 +0800

    Show Loading Skeletons

--- src/components/PokeGrid.tsx
+++ src/components/PokeGrid.tsx
@@ -1,19 +1,22 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import usePokemons from "../hooks/usePokemons";
 import PokeCard from "./PokeCard";
+import PokeCardSkeleton from "./PokeCardSkeleton";
 
 const PokeGrid = () => {
-  const { pokemons, error } = usePokemons();
+  const { pokemons, error, isLoading } = usePokemons();
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 4 }}
+        columns={{ sm: 1, md: 2, xl: 4 }}
         spacing={10}
         padding={"10px"}
       >
         {pokemons.map((pokemon) => (
           <PokeCard key={pokemon.name} pokemon={pokemon} />
         ))}
+        {isLoading && skeleton.map((s) => <PokeCardSkeleton key={s} />)}
       </SimpleGrid>
       {error && <Text>{error}</Text>}
     </>
--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -11,19 +11,23 @@ interface FetchPokemonsResponse {
 const usePokemons = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
     apiClient
       .get<FetchPokemonsResponse>("/pokemon", { signal: controller.signal })
       .then((response) => {
         setPokemons(response.data.results);
+        setIsLoading(false);
       })
       .catch((error) => {
         if (error.name === "CanceledError") return;
         setError(error.message);
+        setIsLoading(false);
       });
     return () => controller.abort();
   }, []);
-  return { pokemons, error };
+  return { pokemons, error, isLoading };
 };
 export default usePokemons;
