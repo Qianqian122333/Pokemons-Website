756941faff3c3239160bf563a2df1bd50adbf8bf
Qianqian Wei <qianqianwei112233@gmail.com>
2026-01-19 13:40:48 +0800

    Display Pokemon Type

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,28 +1,62 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  Badge,
+  HStack,
+} from "@chakra-ui/react";
 import { Pokemon } from "../hooks/usePokemons";
 import { useEffect, useState } from "react";
+
 interface PokeCardProps {
   pokemon: Pokemon;
 }
 
+interface PokemonDetail {
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+}
+
 const PokeCard = ({ pokemon }: PokeCardProps) => {
-  const [img, setImg] = useState("");
+  const [details, setDetails] = useState<PokemonDetail | null>(null);
+
   useEffect(() => {
-    // 这里直接使用 name，不需要知道 ID 是多少
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+    fetch(pokemon.url)
       .then((res) => res.json())
-      .then((data) => {
-        setImg(
-          data.sprites.other["official-artwork"].front_default ||
-            data.sprites.front_default,
-        );
-      });
-  }, [pokemon]);
+      .then((data) => setDetails(data));
+  }, [pokemon.url]);
+
   return (
     <Card borderRadius={10}>
-      <Image src={img} alt={pokemon.name} />
+      <Image
+        src={details?.sprites.other["official-artwork"].front_default}
+        alt={pokemon.name}
+      />
       <CardBody>
         <Heading fontSize="2xl">{pokemon.name}</Heading>
+        <HStack mt={2}>
+          {details?.types.map((t) => (
+            <Badge
+              key={t.type.name}
+              fontSize="14px"
+              paddingX={2}
+              borderRadius={4}
+            >
+              {t.type.name}
+            </Badge>
+          ))}
+        </HStack>
       </CardBody>
     </Card>
   );
